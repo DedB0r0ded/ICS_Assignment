6a933bc03372b6bf8b5951000cfbf7c5b1e968a3
ALI <aloshpc20@gmail.com>
2025-06-12 23:48:23 +0800

    Add triangle and square shape assembly code

---
+++ square.asm
@@ -0,0 +1,54 @@
+section .data
+    newline db 10
+    char db '#', ' '
+    red_color db 27, '[', '3', '1', 'm'   ; ANSI escape sequence for red
+    reset_color db 27, '[', '0', 'm'      ; Reset color
+
+section .text
+    global _start
+
+_start:
+    ; Set red color
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, red_color
+    mov rdx, 5
+    syscall
+
+    mov r8, 5          ; row counter
+
+row_loop:
+    mov r9, 5          ; column counter
+
+col_loop:
+    ; print '# '
+    mov rax, 1         ; sys_write
+    mov rdi, 1         ; stdout
+    mov rsi, char
+    mov rdx, 2         ; 2 bytes: '#' and ' '
+    syscall
+
+    dec r9
+    jnz col_loop
+
+    ; print newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    dec r8
+    jnz row_loop
+
+    ; Reset color
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, reset_color
+    mov rdx, 4
+    syscall
+
+    ; exit
+    mov rax, 60
+    xor rdi, rdi
+    syscall
---
+++ triangle.asm
@@ -0,0 +1,84 @@
+section .data
+    newline db 10
+    hash db '#'
+    space db ' '
+    red_color db 27, '[', '3', '1', 'm'
+    reset_color db 27, '[', '0', 'm'
+
+section .text
+    global _start
+
+_start:
+    ; Set red color
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, red_color
+    mov rdx, 5
+    syscall
+
+    mov r8, 1          ; row counter starts at 1
+
+row_loop:
+    ; Print leading spaces
+    mov r10, 5
+    sub r10, r8
+
+print_spaces:
+    cmp r10, 0
+    je print_hashes
+    
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, space
+    mov rdx, 1
+    syscall
+    
+    dec r10
+    jmp print_spaces
+
+print_hashes:
+    mov r9, r8
+
+hash_loop:
+    ; print '#'
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hash
+    mov rdx, 1
+    syscall
+
+    ; print space if not last hash
+    dec r9
+    cmp r9, 0
+    je print_newline
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, space
+    mov rdx, 1
+    syscall
+
+    jmp hash_loop
+
+print_newline:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    inc r8
+    cmp r8, 6
+    jne row_loop
+
+    ; Reset terminal color
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, reset_color
+    mov rdx, 4
+    syscall
+
+    ; exit
+    mov rax, 60
+    xor rdi, rdi
+    syscall
